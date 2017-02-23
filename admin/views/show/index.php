b044fb6072141282709e414b8681c1e827fb23b8
mawei <1123265518@qq.com>
2017-02-23 10:56:01 +0800

    时间显示

--- admin/views/show/index.php
+++ admin/views/show/index.php
@@ -52,7 +52,6 @@
                                 }else{
                                     echo $v['stime'].' 至 '.$v['etime'];
                                 }?>
-                                <?= $v['stime'] ?>至 <?= $v['etime'] ?></p>
                                 <div class="theatre-button-group">
                                     <a class="theatre-button" href="<?= Url::toRoute(['ticket/seat','show_id'=>$v['id']]) ?>">售票情况</a>
                                     <a class="theatre-button" href="<?= Url::toRoute(['show/edit','id'=>$v['id']]) ?>">编辑详情</a>
