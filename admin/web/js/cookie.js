33d7263a22b62e0ae0addbfbdcf9c203b087e99e
firexxx@163.com <firexxx@163.com>
2017-02-24 17:17:18 +0800

    记住密码完成

--- admin/web/js/cookie.js
+++ admin/web/js/cookie.js
@@ -1,15 +1,39 @@
-document.write("<script src='jquery.cookie.js'></script>")
-document.write("<script src='jquery.min.js'></script>")
-if($("#loginform-rememberme").is(":checked")){
-	var username = $("#loginform-username").val();
-	var password = $("#loginform-password").val();
-	$.cookie("username",username);
-	$.cookie("password",password);
-	
-}
-if( $.cookie(COOKIE_NAME) ){  
-   $("#loginform-username").val( $.cookie(username) );  
- }  
-if( $.cookie(password) ){  
-   $("#loginform-password").val( $.cookie(password) );  
- }  
+var checkbox = document.getElementById('loginform-rememberme');
+$("#loginform-username").blur(function(){
+	if($("#loginform-username") && $("#loginform-password") && checkbox.checked){
+		var username = $("#loginform-username").val();
+		var password = $("#loginform-password").val();
+		$.cookie("username",username);
+		$.cookie("password",password);
+	}
+	if(!checkbox.checked){
+		$.cookie("username","");
+		$.cookie("password","");
+	}
+})
+$("#loginform-password").blur(function(){
+	if($("#loginform-username") && $("#loginform-password" ) && checkbox.checked){
+		var username = $("#loginform-username").val();
+		var password = $("#loginform-password").val();
+		$.cookie("username",username);
+		$.cookie("password",password);
+	}
+	if(!checkbox.checked){
+		$.cookie("username","");
+		$.cookie("password","");
+	}
+})
+$("#loginform-username").val( $.cookie("username") );  
+$("#loginform-password").val( $.cookie("password") );  
+$("#loginform-rememberme").click(function(){
+	if($("#loginform-username") && $("#loginform-password" ) && checkbox.checked){
+		var username = $("#loginform-username").val();
+		var password = $("#loginform-password").val();
+		$.cookie("username",username);
+		$.cookie("password",password);
+	}
+	if(!checkbox.checked){
+		$.cookie("username","");
+		$.cookie("password","");
+	}
+})
