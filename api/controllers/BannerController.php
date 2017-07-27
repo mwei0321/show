577b529b1e4e2abc0a0a8c639c39fa95c835caff
mawei <1123265518@qq.com>
2017-07-27 17:41:47 +0800

    广告详情

--- api/controllers/BannerController.php
+++ api/controllers/BannerController.php
@@ -31,6 +31,31 @@
         }
 
         /**
+         * 广告详情
+         * @return array
+         * @author MaWei (http://www.phpython.com)
+         * @date 2017年7月27日 下午5:11:01
+        **/
+        function actionGetadvertinfo(){
+            $advertId = \Yii::$app->request->get('advert_id',0);
+
+            $advertInfo = [];
+            if($advertId > 0){
+                //节目详情
+                $advertInfo = \common\models\Advert::find()->where(['id'=>$advertId])->asArray()->one();
+                $advertInfo['advert_id'] = $advertInfo['id'];
+                $advertInfo['cover'] = ImageUrl.$advertInfo['cover'];
+                $advertInfo['ctime'] = date('Y-m-d H:i',$advertInfo['ctime']);
+                unset($advertInfo['id']);
+            }else{
+                $this->_reCode = 440;
+                $this->_reMsg = 'id errors->'.$advertId;
+            }
+
+            return $this->_returnJson($advertInfo);
+        }
+
+        /**
          * 获取启动图片logo
          * @return array
          * @author MaWei (http://www.phpython.com)
